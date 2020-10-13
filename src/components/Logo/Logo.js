20a3daba60f627ec58ee0c43ad46c2bad3d3ccf9
nicocadq <nmachado.camejo@gmail.com>
2020-10-13 16:31:25 -0300

    Added responsive logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import LogoImage from "../../assets/images/logo.svg";
+import LogoImageSM from "../../assets/images/logo-sm.svg";
 
 const Logo = () => {
-  return <img src={LogoImage} alt="Logo" />;
+  return (
+    <picture>
+      <source srcset={LogoImageSM} media="(max-width: 670px)" />
+      <img src={LogoImage} alt="Logo" />
+    </picture>
+  );
 };
 
 export default Logo;
