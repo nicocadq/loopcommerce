0b86a71950a1433e0103be51f36b5291a94a2e8f
nicocadq <nmachado.camejo@gmail.com>
2020-10-29 14:54:25 -0300

    Refactored some test suites

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ import styles from "./Header.module.scss";
 
 const Header = ({ children }) => {
   return (
-    <header className={styles.header} data-testid="header">
+    <header className={styles.header}>
       <div className={styles.container}>
         <Link to="/">
           <Logo />
