27aa2dcf10918e39447faf59014d98bff619d1e1
nicocadq <nmachado.camejo@gmail.com>
2020-11-12 15:00:12 -0300

    Added code splitting in navigation routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 import ProtectedRoute from "./components/ProtectedRoute";
+import Loader from "./components/Loader";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 import setupStore from "./store";
-import HomePage from "./pages/HomePage";
-import SignupPage from "./pages/SignupPage";
-import LoginPage from "./pages/LoginPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import ThankYouPage from "./pages/ThankYouPage";
-import CartPage from "./pages/CartPage";
 
 import "./App.scss";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const SignupPage = lazy(() => import("./pages/SignupPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const ProductDetailPage = lazy(() => import("./pages/ProductDetailPage"));
+const ThankYouPage = lazy(() => import("./pages/ThankYouPage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
 
 const { store, persistor } = setupStore();
 
@@ -22,26 +25,30 @@ const App = () => {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <Switch>
-            <Route path="/product/:id">
-              <ProductDetailPage />
-            </Route>
-            <Route path="/signup">
-              <SignupPage />
-            </Route>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <ProtectedRoute path="/thank-you-page">
-              <ThankYouPage />
-            </ProtectedRoute>
-            <ProtectedRoute path="/cart">
-              <CartPage />
-            </ProtectedRoute>
-            <Route path="/">
-              <HomePage />
-            </Route>
-          </Switch>
+          <ErrorBoundary>
+            <Suspense fallback={<Loader />}>
+              <Switch>
+                <Route path="/product/:id">
+                  <ProductDetailPage />
+                </Route>
+                <Route path="/signup">
+                  <SignupPage />
+                </Route>
+                <Route path="/login">
+                  <LoginPage />
+                </Route>
+                <ProtectedRoute path="/thank-you-page">
+                  <ThankYouPage />
+                </ProtectedRoute>
+                <ProtectedRoute path="/cart">
+                  <CartPage />
+                </ProtectedRoute>
+                <Route path="/">
+                  <HomePage />
+                </Route>
+              </Switch>
+            </Suspense>
+          </ErrorBoundary>
         </BrowserRouter>
       </PersistGate>
     </Provider>
